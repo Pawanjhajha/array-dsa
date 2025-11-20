ec9dff54a68c26099b8a15af5bdd2800dbf6845c
Pawan Jhajharia <pawankumarjhajharia3@gmail.com>
2025-11-20 22:59:31 +0530

    sub array

---
+++ maximum-subarray-sum.js
@@ -0,0 +1,13 @@
+let arr=[-2,-3,4,-1,-2,2,5,-3]
+maximu=0
+for(let i=0;i<arr.length;i++){
+    sum=0
+    for(let j=i;j<arr.length;j++){
+        sum=sum+arr[j]
+        maximu=Math.max(sum,maximu)
+    }
+    sum=0
+}
+console.log(maximu)
+
+//T.C O(n*N)

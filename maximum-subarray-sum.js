4c5e78c93cb24ed4e443d7290644230988e54777
Pawan Jhajharia <pawankumarjhajharia3@gmail.com>
2025-11-23 21:40:01 +0530

    optimal approch

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,15 +1,27 @@
-let arr=[-2,-3,4,-1,-2,2,5,-3]
-maximu=0
-for(let i=0;i<arr.length;i++){
-    sum=0
-    for(let j=i;j<arr.length;j++){
-        sum=sum+arr[j]
-        maximu=Math.max(sum,maximu)
-    }
-    sum=0
-}
-console.log(maximu)
+let arr=[-2,-3,4,-1,-2,1,5,-3]
+// maximu=0
+// for(let i=0;i<arr.length;i++){
+//     sum=0
+//     for(let j=i;j<arr.length;j++){
+//         sum=sum+arr[j]
+//         maximu=Math.max(sum,maximu)
+//     }
+//     sum=0
+// }
+// console.log(maximu)
 
 //T.C O(n*N)
 
+maximum=Number.MIN_VALUE //take minum value
+sum=0
+for(let i=0;i<arr.length;i++){
+    sum=sum+arr[i]
+    if(sum<0){
+        sum=0
+    }
+    if(maximum<sum){
+        maximum=sum
+    }
+}
+console.log(maximum,"maximum")
 

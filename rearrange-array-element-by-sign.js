41c5f988598884dbbc6739e56f37f5936918371f
Pawan Jhajharia <pawankumarjhajharia3@gmail.com>
2025-11-26 21:33:19 +0530

    up

--- rearrange-array-element-by-sign.js
+++ rearrange-array-element-by-sign.js
@@ -1,4 +1,4 @@
-let arr=[3,1,-2,-5,2,-4]
+let arr=[3,1,-2,-5,2,-4] //ther is postive and negetive are equal
 
 //answer=[3,1,-2,-5,2,-4]
 //brute force approch
@@ -32,18 +32,64 @@ let arr=[3,1,-2,-5,2,-4]
 
 //t.c O(n)+O(n)+O(n)=O(3n)
 
-let even=0
-let odd=1
-let ans=[]
-for(let i=0;i<arr.length;i++){
-    if(arr[i]>0){
-        //posositve
-        ans[even]=arr[i]
-        even=even+2
+// let even=0 //event index
+// let odd=1 //odd index
+// let ans=[]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>0){
+//         //posositve
+//         ans[even]=arr[i]
+//         even=even+2
+//     }else{
+//         //negetive
+//         ans[odd]=arr[i]
+//         odd=odd+2
+//     }
+// }
+// console.log(ans,"and")
+
+let arr1=[-1,2,3,4,-3,1]
+//use brute force since the postive and negetive are not equal there is two case 
+//1.positive>negetive ,2.negetive>positive
+let positive=[]
+let negetive=[]
+for(let i=0;i<arr1.length;i++){
+    if(arr1[i]>0){
+        positive.push(arr1[i])
     }else{
-        //negetive
-        ans[odd]=arr[i]
-        odd=odd+2
+        negetive.push(arr1[i])
     }
 }
-console.log(ans,"and")
+console.log(positive,negetive)
+
+
+let even=0
+let odd=1
+if(positive.length>negetive.length){
+    for(let i=0;i<negetive.length;i++){
+        arr1[even]=positive[i]
+        arr1[odd]=negetive[i]
+        even=even+2
+        odd=odd+2
+    }
+    let index=negetive.length*2//from here to start
+    for(let i=negetive.length;i<positive.length;i++){
+        arr1[index]=positive[i]
+        index++
+    }
+}else{
+    for(let i=0;i<positive.length;i++){
+        arr1[even]=positive[i]
+        arr1[odd]=negetive[i]
+        even=even+2
+        odd=odd+2
+    }
+    let index=positive.length*2//from here to start
+    for(let i=positive.length;i<negetive.length;i++){
+        arr1[index]=negetive[i]
+        index++
+    }
+}
+
+
+console.log(arr1)
